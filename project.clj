944edce7e95624361a1ca1136ec881620fcefd1d
ntestoc3 <ntestoc3@outlook.com>
2020-02-06 23:44:36 +0800

    去掉多余的jfx依赖，减少uberjar体积，添加openjfx mac支持依赖

--- project.clj
+++ project.clj
@@ -5,15 +5,17 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [me.raynes/fs "1.4.6"] ;; file util
-                 [cljfx "1.6.3"] ;; javafx gui
-                 [org.clojure/core.cache "0.8.2"] ;; cache
+                 [cljfx "1.6.3" :exclusions [[org.openjfx/javafx-media]
+                                             [ org.openjfx/javafx-web]]] ;; javafx gui
+                 [org.clojure/core.cache "0.8.2"]  ;; cache
                  [net.lingala.zip4j/zip4j "2.3.1"] ;; zip with password
-                 [com.taoensso/timbre "4.10.0"] ; logging
-                 [clojure.java-time "0.3.2"] ; datetime
-                 [cprop/cprop "0.1.15"] ;; env manage
+                 [com.taoensso/timbre "4.10.0"]    ; logging
+                 [clojure.java-time "0.3.2"]       ; datetime
+                 [cprop/cprop "0.1.15"]            ;; env manage
 
                  ;; 添加JavaFX的windows系统支持, mac如果要支持也需要添加依赖
                  [org.openjfx/javafx-graphics "13" :classifier "win"]
+                 [org.openjfx/javafx-graphics "13" :classifier "mac"]
                  ]
   :omit-source true
   :main ^:skip-aot maze-tower.core
